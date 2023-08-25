ad0b6d18931e1f602dedb5e5ae9660501c55c020
lawrence910426 <lawrence910426@gmail.com>
2023-08-26 02:54:22 +0800

    feat: include db ranking

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const OpenAIApi = require("openai");
 const openai = new OpenAIApi({
 	apiKey: process.env.OPENAI_API_KEY
 });
-
+const xlsx = require('node-xlsx')
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
@@ -11,11 +11,18 @@ const token = process.env.TELEGRAM_KEY;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
+// Load the database
+const worksheet = xlsx.parse(`${__dirname}/db.xlsx`)[0].data.slice(1)
+const sentences = worksheet.map((row) => row.join("\t"))
+const MAX_TOKENS = 1000;
+
+const fetch = require("node-fetch");
+
 var history = [];
 
 
 bot.onText(/\/clear/, async (msg, match) => {
-	history = [];
+  history = [];
 })
 
 bot.onText(/\/ask (.+)/, async (msg, match) => {
@@ -28,8 +35,9 @@ bot.onText(/\/ask (.+)/, async (msg, match) => {
 
 
 const reply = async (content) => {
+  var prmpt = await similarity(content) + "\n" + history.join("\n") + "\nQuestion: " + content + "\nAnswer: ";
+  console.log("Prompt\n============\n" + prmpt)
   history.push(content);
-  prmpt = history.join("\n")
 
   const completion = await openai.chat.completions.create({
     messages: [{ role: 'user', content: prmpt }],
@@ -42,3 +50,41 @@ const reply = async (content) => {
   return reply
 }
 
+const query = async (data) => {
+  const response = await fetch(
+      "https://api-inference.huggingface.co/models/sentence-transformers/msmarco-distilbert-base-tas-b",
+      {
+          headers: { Authorization: `Bearer ${process.env.HUGGING_FACE_KEY}` },
+          method: "POST",
+          body: JSON.stringify(data),
+      }
+  );
+  const result = await response.json();
+  return result;
+}
+
+const similarity = async (question) => {
+  var response = await query({
+    "inputs": {
+        "source_sentence": question,
+        "sentences": sentences
+    },
+    "wait_for_model": true
+  })
+
+  const sorted = response.map((item, i) => {
+    return { index: i, value: item, sentence: sentences[i] };
+  }).sort((a, b) => {
+    return b.value - a.value;
+  })
+  console.log(sorted)
+
+  var prompts = []
+  var i = 0;
+  for(var token_length = 0; token_length < MAX_TOKENS; token_length += sentences[sorted[i].index].length) {
+    prompts.push(sentences[sorted[i].index])
+    i += 1;
+    if (i >= sorted.length) break;
+  }
+  return prompts.join("\n")
+}
